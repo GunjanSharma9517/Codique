4ff8ff761b315620af8a4095feb6241503d5e615
GunjanSharma9517 <79080854+GunjanSharma9517@users.noreply.github.com>
2021-03-09 18:09:12 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -9,14 +9,21 @@ app = Flask(__name__)
 @app.route('/',methods=['GET','POST'])
 def fetechdata():
     if request.method == 'POST':
-        text = request.form['htmlinput']
+        mname = request.form['managername']
+        pname = request.form['projectname']
+        nors = request.form['prjdesc']
         # try:
-        get_project_details(text)
+        get_project_details(mname,pname,nors)
              # message= 'Created tables.Created files.'
         # except:
             # message='Tables already exist'
-        return render_template('dashboard.html',text=text)
+        return render_template('dashboard.html',mname=mname,pname=pname,nors=nors)
     elif request.method == 'GET':
         return render_template('dashboard.html')
+        
+        
 if __name__ == "__main__":
     app.run(debug=True)
+    
+    
+    
