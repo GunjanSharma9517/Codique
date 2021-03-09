c14c2311a57686cf789701eb869d37b9cee0f9eb
GunjanSharma9517 <79080854+GunjanSharma9517@users.noreply.github.com>
2021-03-09 18:09:31 +0530

    Add files via upload

--- get_details.py
+++ get_details.py
@@ -7,11 +7,11 @@ import os
 # conn = sqlite3.connect(sqlite_file)
 # c = conn.cursor()
 
-def get_project_details(project_desc):
+def get_project_details(project_pname,project_mname,project_resource):
     sqlite_file='.\SQLiteStudio-3.2.1\SQLiteStudio\Db_codique.db'
     conn = sqlite3.connect(sqlite_file)
     c = conn.cursor()
-    ins_query = """INSERT INTO Project_Details (projectManager) VALUES (:desc);"""
-    c.execute(ins_query, {"desc": project_desc})
+    ins_query = """INSERT INTO Project_Details (projectManager,projectName,NumOfResources) VALUES (:mname,:pname,:nors);"""
+    c.execute(ins_query, {"mname": project_pname,"pname":project_mname,"nors":project_resource})
     conn.commit()
     return(print('recored inserted'))
